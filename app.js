6d8379a8d05033eaf5ef27a468fe253fe93ebc1f
Adam <arosini@andrew.cmu.edu>
2013-04-04 18:44:54 -0400

    Cleaned up

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ app.get("/static/:staticFilename", function (request, response) {
   response.sendfile("static/" + request.params.staticFilename);
 });
 
-app.get("/THREEexample/:staticFilename", function (request, response) {
-  response.sendfile("THREEexample/" + request.params.staticFilename);
-});
 
 app.listen(8889);
 
@@ -20,16 +17,17 @@ app.listen(8889);
 // === Socket.io server ===
 // ========================
 
-var io = require("socket.io").listen(8888);
+var io = require("socket.io").listen(8888,{ log: false });
 
 io.sockets.on("connection", function (socket) {
   socket.on("coords", function (data) {
     //socket.emit("status", {success: "true"});
     io.sockets.emit("drawcoords", {x: data.x + 20, y: data.y + 20});
   });
-  
-  socket.on("sendSphere", function (data) {
-   socket.broadcast.emit("receiveSphere", {player:socket.id, position:data});
+ 
+
+  socket.on("sendPosition", function (data) {
+   socket.broadcast.emit("receivePosition", {player:socket.id, position:data});
   });
   
 });
